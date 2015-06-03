a351e6e5b7b9e2cb9aa6e6fb0effeba289cc3844
Bill Clinton <BillClinton@users.noreply.github.com>
2015-06-03 16:08:47 -0400

    added jarvus-ext-actionevents and jarvus-ext-glyphs packages to .gitignore and get-packages.sh

--- sencha-workspace/packages/get-packages.sh
+++ sencha-workspace/packages/get-packages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git clone -b ext-5.1.1.451 https://github.com/JarvusInnovations/jarvus-ext-routing.git
 git clone -b ext-5.1.1.451 https://github.com/JarvusInnovations/sencha-hotfixes.git jarvus-hotfixes
+git clone https://github.com/JarvusInnovations/jarvus-ext-glyphs.git
+git clone https://github.com/JarvusInnovations/jarvus-ext-actionevents.git
 
