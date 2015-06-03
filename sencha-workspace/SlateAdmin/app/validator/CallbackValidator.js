3941d2b0d69ad2b140076ecd0db5abc6b780f743
Bill Clinton <BillClinton@users.noreply.github.com>
2015-06-03 13:25:37 -0400

    added record to callback function, cleaned up code, added header comment

--- sencha-workspace/SlateAdmin/app/validator/CallbackValidator.js
+++ sencha-workspace/SlateAdmin/app/validator/CallbackValidator.js
@@ -1,6 +1,9 @@
 /*jslint browser: true, undef: true *//*global Ext*/
 /**
- * Validates that the passed value is not `null` or `undefined` or `''`.
+ * Custom validator that allows validation by a function specified in the validator configuration.
+ *
+ * This current requires fixes to Ext.data.Validation and Ext.data.field.Field contained in
+ * the ext-5.1.1.451 branch of JarvusInnovations/sencha-hotfixes
  */
 Ext.define('SlateAdmin.validator.CallbackValidator', {
     extend: 'Ext.data.validator.Validator',
@@ -16,18 +19,16 @@ Ext.define('SlateAdmin.validator.CallbackValidator', {
         message: 'not valid',
 
         /**
-         * @cfg {Function} fn
-         * `true` to allow `''` as a valid value.
+         * @cfg {Function} callback
          */
-        callback: null,
-        allowEmpty: false
+        callback: null
     },
 
-    validate: function(val) {
+    validate: function(val,rec) {
         var cb = this.getCallback();
 
         if (cb !== null && typeof cb === 'function') {
-            return cb(val) ? true : this.getMessage();
+            return cb(val,rec) ? true : this.getMessage();
         }
         return 'CallbackValidator requires callback config parameter to be a function';
     }
