c85fe904709f4c31d22fea22897c91046f2285fa
Bill Clinton <BillClinton@users.noreply.github.com>
2015-06-03 15:17:13 -0400

    script to get all packages from their repositories.  We may not need this eventually, but for now is at least useful for keeping track of which packages have been moved to their own repositories

--- sencha-workspace/packages/get-packages.sh
+++ sencha-workspace/packages/get-packages.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 git clone -b ext-5.1.1.451 https://github.com/JarvusInnovations/jarvus-ext-routing.git
+git clone -b ext-5.1.1.451 https://github.com/JarvusInnovations/sencha-hotfixes.git jarvus-hotfixes
+
