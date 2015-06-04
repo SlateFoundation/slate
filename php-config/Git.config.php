3d39d3c3c5628c3f1808542ac399ba0e266fc56c
Chris Alfano <chris@jarv.us>
2015-06-04 15:56:45 -0400

    modernized stock Git.config.php

--- php-config/Git.config.php
+++ php-config/Git.config.php
@@ -1,19 +1,22 @@
 <?php
 
-Git::$repositories['slatefoundation-v1'] = array(
-    'remote' => 'git@github.com:SlateFoundation/slate.git'
-    ,'originBranch' => 'master'
-    ,'workingBranch' => 'v1.slate.is'
-    ,'localOnly' => true
-    ,'trees' => array(
-        'ext-library'
-        ,'html-templates'
-        ,'php-classes'
-        ,'php-config'
-        ,'php-migrations'
-        ,'phpunit-tests'
-        ,'sencha-workspace'
-        ,'sencha-docs'
-        ,'site-root'
-    )
-);
+Git::$repositories['slate'] = [
+    'remote' => 'git@github.com:SlateFoundation/slate.git',
+    'originBranch' => 'master',
+    'workingBranch' => 'instances/' . Site::getConfig('primary_hostname'),
+    'localOnly' => true,
+    'trees' => [
+        'dwoo-plugins',
+        'event-handlers',
+        'ext-library',
+        'html-templates',
+        'mail-handlers',
+        'php-classes',
+        'php-config',
+        'php-migrations',
+        'phpunit-tests',
+        'sencha-docs',
+        'sencha-workspace',
+        'site-root'
+    ]
+];
