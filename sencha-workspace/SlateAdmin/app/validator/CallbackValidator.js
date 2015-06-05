e10594ffb1bda187aad7601c13b752d42f8da4e9
Bill Clinton <BillClinton@users.noreply.github.com>
2015-06-05 04:06:50 -0400

    cleaned up documentation

--- sencha-workspace/SlateAdmin/app/validator/CallbackValidator.js
+++ sencha-workspace/SlateAdmin/app/validator/CallbackValidator.js
@@ -2,28 +2,50 @@
 /**
  * Custom validator that allows validation by a function specified in the validator configuration.
  *
- * This current requires fixes to Ext.data.Validation and Ext.data.field.Field contained in
- * the ext-5.1.1.451 branch of JarvusInnovations/sencha-hotfixes
+ * For the callback function to receive the rec parameter, fixes to Ext.data.Validation and Ext.data.field.Field
+ * contained in the ext-5.1.1.451 branch of JarvusInnovations/sencha-hotfixes are currently required
+ *
+ * Example usage in model validators config:
+ *
+ *     @example
+ *     validators: {
+ *         passwordConfirmation: {
+ *             type: 'callback',
+ *             message: 'Passwords must match',
+ *             callback: function(val, rec) {
+ *                 return (val === rec.get('password');
+ *             }
+ *         }
+ *     }
  */
 Ext.define('SlateAdmin.validator.CallbackValidator', {
     extend: 'Ext.data.validator.Validator',
     alias: 'data.validator.callback',
 
+    /**
+     * @property type="callback"
+     */
     type: 'callback',
 
     config: {
         /**
-         * @cfg {String} message
-         * The error message to return when the value is not specified.
+         * @cfg {String} message="not valid"
+         * The error message to return when the callback function does not return true.
          */
         message: 'not valid',
 
         /**
-         * @cfg {Function} callback
+         * @cfg {Function} callback (required)
+         * The callback function used to evaluate the validity of the field.
          */
         callback: null
     },
 
+    /**
+     * Uses the callback function to determine the validity of the field.
+     * @param val the field's value
+     * @param rec the field's parent record
+     */
     validate: function(val,rec) {
         var cb = this.getCallback();
 
