04a3c432c0828b7f16b9cf5af7aeb77727866f7d
Chris Alfano <chris@jarv.us>
2015-06-11 15:59:01 -0400

    removed breadcrumb mockup code

--- sencha-workspace/SlateAdmin/app/view/Viewport.js
+++ sencha-workspace/SlateAdmin/app/view/Viewport.js
@@ -2,21 +2,12 @@
 Ext.define('SlateAdmin.view.Viewport', {
     extend: 'Ext.container.Viewport',
     requires: [
-        'SlateAdmin.view.Breadcrumbs',
         'SlateAdmin.view.Navigation',
         'Ext.layout.container.Card'
     ],
 
     layout: 'border',
     items: [{
-/*
-        region: 'north',
-        layout: 'fit',
-        items: [{
-            xtype: 'slateadmin-breadcrumbs'
-        }]
-    },{
-*/
         region: 'west',
         xtype: 'slateadmin-navigation',
         split: true,
