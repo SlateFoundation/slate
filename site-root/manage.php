9fd6af0888c9ebdd0ac30cd7c7918c2e6059a4e0
Chris Alfano <chris@jarv.us>
2015-06-11 10:45:36 -0400

    moved manage.php from slate to slate-admin package

--- site-root/manage.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$GLOBALS['Session']->requireAccountLevel('Staff');
-
-Sencha_RequestHandler::respond('app/SlateAdmin/ext', array(
-    'App' => Sencha_App::getByName('SlateAdmin')
-    ,'mode' => 'production'
-));
