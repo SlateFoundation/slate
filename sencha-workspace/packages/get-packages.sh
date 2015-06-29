54f4c7c4f579e7203242d5ef271b9be06993f5a1
Chris Alfano <chris@jarv.us>
2015-06-28 23:12:09 -0400

    sorted get-packages to match app.json

--- sencha-workspace/packages/get-packages.sh
+++ sencha-workspace/packages/get-packages.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-git clone -b ext/5/1/1/451 https://github.com/JarvusInnovations/jarvus-ext-routing.git
-git clone -b ext/5/1/1/451 https://github.com/JarvusInnovations/sencha-hotfixes.git jarvus-hotfixes
-git clone https://github.com/JarvusInnovations/jarvus-apikit.git
-git clone https://github.com/JarvusInnovations/emergence-apikit.git
-git clone -b ext/5/1/1/451 https://github.com/JarvusInnovations/jarvus-lazydata.git
-git clone https://github.com/JarvusInnovations/jarvus-ext-glyphs.git
-git clone https://github.com/JarvusInnovations/jarvus-ext-actionevents.git
-git clone https://github.com/JarvusInnovations/jarvus-ext-treerecords.git
-git clone https://github.com/JarvusInnovations/jarvus-ext-searchfield.git
+
+# app theme
 git clone https://github.com/SlateFoundation/slate-theme.git slate-theme
+
+# framework hotfixes
+git clone -b ext/5/1/1/451 https://github.com/JarvusInnovations/sencha-hotfixes.git jarvus-hotfixes
+
+# feature packages
+git clone https://github.com/JarvusInnovations/emergence-apikit.git
+git clone https://github.com/JarvusInnovations/jarvus-apikit.git
+git clone https://github.com/JarvusInnovations/jarvus-ext-searchfield.git
+git clone -b ext/5/1/1/451 https://github.com/JarvusInnovations/jarvus-ext-routing.git
+git clone -b ext/5/1/1/451 https://github.com/JarvusInnovations/jarvus-lazydata.git
+git clone https://github.com/JarvusInnovations/jarvus-ext-actionevents.git
+git clone https://github.com/JarvusInnovations/jarvus-ext-glyphs.git
+git clone https://github.com/JarvusInnovations/jarvus-ext-treerecords.git
