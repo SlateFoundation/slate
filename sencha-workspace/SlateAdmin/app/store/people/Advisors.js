f4896faaec2b1b8123b3e97b51e16bdf0f985d18
Anthony Harley <aharley40@gmail.com>
2015-09-10 16:10:31 -0400

    Remove useless proxy reader

--- sencha-workspace/SlateAdmin/app/store/people/Advisors.js
+++ sencha-workspace/SlateAdmin/app/store/people/Advisors.js
@@ -1,14 +1,10 @@
 /*jslint browser: true, undef: true, white: false, laxbreak: true *//*global Ext,Slate*/
 Ext.define('SlateAdmin.store.people.Advisors', {
     extend: 'Ext.data.Store',
-    
+
     model: 'SlateAdmin.model.person.Person',
     proxy: {
         type: 'slaterecords',
-        url: '/advisors/json',
-        reader: {
-            type: 'json',
-            rootProperty: 'data'
-        }
+        url: '/advisors'
     }
 });
