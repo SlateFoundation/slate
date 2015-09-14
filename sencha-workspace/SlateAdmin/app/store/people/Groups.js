af9474c4ed33c44be4f410fdb698bab70337a308
Chris Alfano <chris@jarv.us>
2015-09-14 14:09:53 -0400

    Move groups post-processor to store onProxyLoad and initial sort to config

--- sencha-workspace/SlateAdmin/app/store/people/Groups.js
+++ sencha-workspace/SlateAdmin/app/store/people/Groups.js
@@ -3,6 +3,7 @@ Ext.define('SlateAdmin.store.people.Groups', {
     extend: 'Ext.data.Store',
 
     model: 'SlateAdmin.model.person.Group',
+    sorters: 'Left',
     proxy: {
         type: 'slaterecords',
         url: '/groups',
@@ -11,6 +12,21 @@ Ext.define('SlateAdmin.store.people.Groups', {
         include: 'Population',
         extraParams: {
             parentGroup: 'any'
+        }
+    },
+
+    onProxyLoad: function(operation) {
+        var me = this,
+            i = 0, count, group, parentGroup;
+
+        me.callParent(arguments);
+
+        if (operation.wasSuccessful()) {
+            for (count = me.getCount(); i < count; i++) {
+                group = me.getAt(i);
+                parentGroup = me.getById(group.get('ParentID'));
+                group.set('namesPath', (parentGroup ? parentGroup.get('namesPath') : '') + '/' + group.get('Name'));
+            }
         }
     }
 });
