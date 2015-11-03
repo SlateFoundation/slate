47544cca03806af1c12fbf7e2babab485dce63ae
Chris Alfano <chris@jarv.us>
2015-11-02 19:46:02 -0500

    Bulk syntax updates

--- php-classes/Slate/Progress/Narratives/Report.php
+++ php-classes/Slate/Progress/Narratives/Report.php
@@ -6,91 +6,89 @@ use DuplicateKeyException;
 
 class Report extends \VersionedRecord
 {
-    
     // VersionedRecord configuration
-    static public $historyTable = 'history_narrative_reports';
+    public static $historyTable = 'history_narrative_reports';
 
     // ActiveRecord configuration
-    static public $tableName = 'narrative_reports';
-    static public $singularNoun = 'narrative report';
-    static public $pluralNoun = 'narrative reports';
-    static public $updateOnDuplicateKey = true;
-    static public $trackModified = true;
-    
+    public static $tableName = 'narrative_reports';
+    public static $singularNoun = 'narrative report';
+    public static $pluralNoun = 'narrative reports';
+    public static $updateOnDuplicateKey = true;
+    public static $trackModified = true;
+
     // required for shared-table subclassing support
-    static public $rootClass = __CLASS__;
-    static public $defaultClass = __CLASS__;
-    static public $subClasses = array(__CLASS__);
+    public static $defaultClass = __CLASS__;
+    public static $subClasses = [__CLASS__];
 
-    static public $fields = array(
-        'StudentID' => array(
-            'type' => 'integer'
-            ,'unsigned' => true
-        )
-        ,'CourseSectionID' => array(
-            'type' => 'integer'
-            ,'unsigned' => true
-        )
-        ,'TermID' => array(
-            'type' => 'integer'
-            ,'unsigned' => true
-        )
+    public static $fields = [
+        'StudentID' => [
+            'type' => 'integer',
+            'unsigned' => true
+        ],
+        'CourseSectionID' => [
+            'type' => 'integer',
+            'unsigned' => true
+        ],
+        'TermID' => [
+            'type' => 'integer',
+            'unsigned' => true
+        ],
 
-        ,'Status' => array(
-            'type' => 'enum'
-            ,'values' => array('Draft','Published')
-            ,'default' => 'Draft'
-        )
-        ,'Updated' => array(
+        'Status' => [
+            'type' => 'enum',
+            'values' => ['Draft','Published'],
+            'default' => 'Draft'
+        ],
+        'Updated' => [
             'type' => 'timestamp'
             ,'notnull' => false
-        )
+        ],
 
         // TODO: comment out so sites can configure
-        ,'Grade' => array(
-            'type' => 'enum'
-            ,'values' => array('A','B','C','D','F','Inc')
-            ,'notnull' => false
-        )
-        ,'Assessment' => array(
-            'type' => 'clob'
-            ,'notnull' => false
-        )
-        ,'Comments' => array(
-            'type' => 'clob'
-            ,'notnull' => false
-        )
-    );
-    
-    
-    static public $indexes = array(
-        'NarrativeReport' => array(
-            'fields' => array('StudentID','CourseSectionID','TermID')
-            ,'unique' => true
-        )
-    );
-    
-    static $relationships = array(
-        'Section' => array(
-            'type' => 'one-one'
-            ,'class' => \Slate\Courses\Section::class
-            ,'local' => 'CourseSectionID'
-        )
-        ,'Student' => array(
-            'type' => 'one-one'
-            ,'class' => \Slate\People\Student::class
-        )
-        ,'Term' => array(
-            'type' => 'one-one'
-            ,'class' => \Slate\Term::class
-        )
-    );
+        'Grade' => [
+            'type' => 'enum',
+            'values' => ['A','B','C','D','F','Inc'],
+            'notnull' => false
+        ],
+        'Assessment' => [
+            'type' => 'clob',
+            'notnull' => false
+        ],
+        'Comments' => [
+            'type' => 'clob',
+            'notnull' => false
+        ]
+    ];
+
+
+    public static $indexes = [
+        'NarrativeReport' => [
+            'fields' => ['StudentID', 'CourseSectionID', 'TermID'],
+            'unique' => true
+        ]
+    ];
+
+    public static $relationships = [
+        'Section' => [
+            'type' => 'one-one',
+            'class' => \Slate\Courses\Section::class,
+            'local' => 'CourseSectionID'
+        ],
+        'Student' => [
+            'type' => 'one-one',
+            'class' => \Slate\People\Student::class
+        ],
+        'Term' => [
+            'type' => 'one-one',
+            'class' => \Slate\Term::class
+        ]
+    ];
 
     public function validate($deep = true)
     {
         // call parent
         parent::validate($deep);
-        
+
         $this->_validator->validate([
             'field' => 'Grade',
             'validator' => 'selection',
@@ -98,7 +96,7 @@ class Report extends \VersionedRecord
             'required' => ($this->Status=='Published'),
             'errorMessage' => 'Grade is require before publishing'
         ]);
-        
+
         // save results
         return $this->finishValidation();
     }
