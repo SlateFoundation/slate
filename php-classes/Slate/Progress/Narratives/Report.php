21db702e13c755b0a01276c4bbb7e432caaacad9
Chris Alfano <chris@jarv.us>
2015-11-02 19:37:07 -0500

    Basic updates for report model

--- php-classes/Slate/Progress/Narratives/Report.php
+++ php-classes/Slate/Progress/Narratives/Report.php
@@ -6,6 +6,7 @@ use DuplicateKeyException;
 
 class Report extends \VersionedRecord
 {
+    
     // VersionedRecord configuration
     static public $historyTable = 'history_narrative_reports';
 
@@ -13,6 +14,8 @@ class Report extends \VersionedRecord
     static public $tableName = 'narrative_reports';
     static public $singularNoun = 'narrative report';
     static public $pluralNoun = 'narrative reports';
+    static public $updateOnDuplicateKey = true;
+    static public $trackModified = true;
     
     // required for shared-table subclassing support
     static public $rootClass = __CLASS__;
@@ -83,142 +86,20 @@ class Report extends \VersionedRecord
         )
     );
 
-    public function getData()
-    {       
-        // embed related object(s)
-        $WorksheetAssignment = StandardsWorksheetAssignment::getByWhere(array(
-            'TermID' => $this->TermID
-            ,'CourseSectionID' => $this->CourseSectionID
-        ));
-        
-        $prompts = array();
-        
-        if($WorksheetAssignment)
-        {
-            $promptIDs = \DB::allRecords('SELECT PromptID FROM `%s` WHERE WorksheetID=%u', array(
-                WorksheetPrompt::$tableName
-                ,$WorksheetAssignment->WorksheetID
-            ));
-            
-            $promptIDs = array_map(function($record){
-                return $record['PromptID'];
-            }, $promptIDs);
-
-            foreach($promptIDs as $promptID)
-            {
-                $Grade = PromptGrade::getByWhere(array(
-                    'StudentID' => $this->StudentID
-                    ,'CourseSectionID' => $this->CourseSectionID
-                    ,'TermID' => $this->TermID
-                    ,'PromptID' => $promptID
-                ));
-                
-                $summarizedPrompt = array(
-                    'PromptID' => $promptID
-                );
-                
-                if($Grade)
-                {
-                    $summarizedPrompt['Grade'] = $Grade->Grade;
-                    $summarizedPrompt['Prompt'] = $Grade->Prompt->Prompt;
-                }
-                else
-                {
-                    $Prompt = Prompt::getByID($promptID);
-                    
-                    $summarizedPrompt['Grade'] = null;
-                    $summarizedPrompt['Prompt'] = $Prompt->Prompt;
-                }
-                
-                array_push($prompts, $summarizedPrompt);    
-            }
-            
-            usort($prompts, function($a, $b) {
-                return strnatcmp($a['Prompt'], $b['Prompt']);
-            });
-        }
-        
-        
-        return array_merge(parent::getData(), array(
-            'Student' => $this->Student ? $this->Student->getData() : null
-            ,'Section' => $this->Section ? $this->Section->getData() : null
-            ,'Worksheet' => $Worksheet
-            ,'Prompts' => $prompts
-            ,'Updated' => $this->Updated ? $this->Updated : $this->Created
-        ));
-    }
-    
     public function validate($deep = true)
     {
         // call parent
         parent::validate($deep);
         
-        $this->_validator->validate(array(
-            'field' => 'Grade'
-            ,'validator' => 'selection'
-            ,'choices' => self::$fields['Grade']['values']
-            ,'required' => ($this->Status=='Published')
-            ,'errorMessage' => 'Grade is require before publishing'
-        ));
+        $this->_validator->validate([
+            'field' => 'Grade',
+            'validator' => 'selection',
+            'choices' => static::getFieldOptions('Grade', 'values'),
+            'required' => ($this->Status=='Published'),
+            'errorMessage' => 'Grade is require before publishing'
+        ]);
         
         // save results
         return $this->finishValidation();
-    }
-    
-    public function getWorksheetAssignment()
-    {
-        return StandardsWorksheetAssignment::getByWhere(array(
-            'CourseSectionID' => $this->CourseSectionID
-            ,'TermID' => $this->TermID
-        ));
-    }
-    
-    public function getStandardsGrades()
-    {
-        $promtpGradClass = str_replace('\\', '_',PromptGrade::class);
-        $where = array(
-            'TermID' => $this->TermID
-            ,'CourseSectionID' => $this->CourseSectionID
-            ,'StudentID' => $this->StudentID
-            ,"EXISTS(SELECT swp.ID FROM standards_worksheet_prompts swp WHERE swp.PromptID = $promtpGradClass.PromptID AND swp.WorksheetID = (SELECT swa.WorksheetID FROM standards_worksheet_assignments swa WHERE swa.TermID = $promtpGradClass.TermID AND swa.CourseSectionID = $promtpGradClass.CourseSectionID))"
-        );
-
-        return PromptGrade::getAllByWhere($where, array(
-            'order' => 
-                '(SELECT CONCAT(LastName,FirstName) FROM people WHERE people.ID = StudentID)'
-                .', CourseSectionID'
-                .', (SELECT Prompt FROM standards_prompts WHERE standards_prompts.ID = PromptID)'
-        ));
-    }
-    
-    public function save($deep = true, $createRevision = true)
-    {
-        
-        if($this->isDirty)
-        {
-            $this->Updated = time();
-        }
-        
-        try
-        {
-            // call parent
-            parent::save($deep, $createRevision);
-        }
-        catch(DuplicateKeyException $e)
-        {
-            // duplicate create save! apply update to existing record
-            $Existing = static::getByWhere(array(
-                'StudentID' => $this->StudentID
-                ,'CourseSectionID' => $this->CourseSectionID
-                ,'TermID' => $this->TermID
-            ));
-            
-            $Existing->setFields($this->_record);
-            
-            $Existing->save();
-            
-            // clone existing record's data
-            $this->_record = $Existing->_record;
-        }
     }
 }
