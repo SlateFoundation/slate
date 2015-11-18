226fa8f067c1901daeab11e9bc2a3cdcada20d1d
Chris Alfano <chris@jarv.us>
2015-11-17 21:40:03 -0500

    Remove unused celleditor and column from narratives section grid

--- sencha-workspace/SlateAdmin/app/view/progress/narratives/SectionsGrid.js
+++ sencha-workspace/SlateAdmin/app/view/progress/narratives/SectionsGrid.js
@@ -4,20 +4,13 @@ Ext.define('SlateAdmin.view.progress.narratives.SectionsGrid', {
     xtype: 'progress-narratives-sectionsgrid',
     requires: [
         'Ext.grid.column.Template',
-        'Ext.form.field.ComboBox',
         'Ext.toolbar.Spacer',
-        'Ext.grid.plugin.CellEditing',
         'Ext.button.Button'
     ],
 
     width: 250,
     store: 'progress.narratives.Sections',
     componentCls: 'progress-narratives-grid',
-    plugins: [{
-        ptype: 'cellediting',
-        clicksToEdit: 2,
-        pluginId: 'narrativeAssignmentEditing'
-    }],
     tbar: [{
         xtype: 'button',
         text: 'My classes only',
@@ -49,49 +42,5 @@ Ext.define('SlateAdmin.view.progress.narratives.SectionsGrid', {
                 '{Code}',
             '</a>'
         ]
-    // },{
-
-    //     header: 'Worksheet',
-    //     flex: 1,
-    //     dataIndex: 'WorksheetID',
-    //     field: {
-    //         xtype: 'combo',
-    //         store: {
-    //             fields: ['ID', 'Title', 'Handle'],
-    //             proxy:{
-    //                 type: 'slateapi',
-    //                 url: '/standards/worksheets',
-    //                 limitParam: false,
-    //                 pageParam: false,
-    //                 startParam: false,
-    //                 reader: {
-    //                     rootProperty: 'data',
-    //                     type: 'json'
-    //                 }
-    //             }
-    //         },
-    //         displayField: 'Title',
-    //         valueField: 'ID',
-    //         triggerAction: 'all',
-    //         typeAhead: true,
-    //         forceSelection: true,
-    //         selectOnFocus: true,
-    //         allowBlank: false,
-    //         emptyText: 'Select worksheet'
-    //     },
-    //     renderer: function (v, metaData,r) {
-    //         if(v) {
-    //             var worksheet = r.get('Worksheet');
-    //             if(worksheet) {
-    //                 return r.get('Worksheet').Title;
-    //             } else {
-    //                 metaData.css = 'x-form-empty-field';
-    //                 return '[Disabled Worksheet]';
-    //             }
-    //         } else {
-    //             metaData.css = 'x-form-empty-field';
-    //             return 'Double-click to assign worksheet';
-    //         }
-    //     }
     }]
 });
