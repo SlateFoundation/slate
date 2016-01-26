78bc5e1688e0c05e853763e899d35e093cf65792
Bill Clinton <BillClinton@users.noreply.github.com>
2016-01-26 14:02:59 -0500

    Add controller for printer functionality

---
+++ sencha-workspace/SlateAdmin/app/controller/progress/Printer.js
@@ -0,0 +1,99 @@
+/*jslint browser: true, undef: true, white: true, laxbreak: true *//*global Ext,SlateAdmin*/
+Ext.define('SlateAdmin.controller.progress.Printer', {
+    extend: 'Ext.app.Controller',
+
+    views: [
+        'progress.narratives.Printer'
+    ],
+
+    stores: [
+        'people.Advisors',
+        'progress.narratives.Sections',
+        'progress.narratives.Students'
+    ],
+
+    routes: {
+        'progress/narratives/printing': 'showNarrativePrinting'
+    },
+
+    refs: {
+        progressNavPanel: 'progress-navpanel',
+
+        narrativesPrinter: {
+            selector: 'progress-narratives-printer',
+            autoCreate: true,
+
+            xtype: 'progress-narratives-printer'
+        },
+        narrativesPrintForm: 'progress-narratives-printer form'
+    },
+
+    control: {
+        narrativesPrinter: {
+            activate: 'onPrinterActivate'
+        },
+        'progress-narratives-printer button[action=clear-filters]': {
+            click: 'onNarrativesClearFiltersClick'
+        },
+        'progress-narratives-printer button[action=preview]': {
+            click: 'onNarrativesPreviewClick'
+        },
+        'progress-narratives-printer button[action=print-pdf]': {
+            click: 'onNarrativesPrintPdfClick'
+        },
+        'progress-narratives-printer button[action=print-browser]': {
+            click: 'onNarrativesPrintBrowserClick'
+        }
+    },
+
+    showNarrativePrinting: function () {
+        this.application.getController('Viewport').loadCard(this.getNarrativesPrinter());
+        //this.application.getController('Viewport').loadCard(this.getManagerCt());
+    },
+
+    onPrinterActivate: function (managerCt) {
+        console.log('onPrinterActivate');
+        var termSelector = this.getNarrativesPrinter().down('combo[name=termID]'),
+            selectedTerm = termSelector.getValue(),
+            termStore = Ext.getStore('Terms'),
+            advisorStore = Ext.getStore('people.Advisors'),
+            onTermLoad = function () {
+                if(!selectedTerm) {
+                    termSelector.setValue(termStore.getReportingTerm().getId());
+                    managerCt.setLoading(false);
+                }
+
+
+            };
+
+        if(!termStore.isLoaded()) {
+            managerCt.setLoading('Loading terms&hellip;');
+            termStore.load({
+                callback: onTermLoad
+            });
+        }
+
+        if(!advisorStore.isLoaded()) {
+            advisorStore.load();
+        }
+    },
+
+    onNarrativesPreviewClick: function () {
+        var formValues = this.getNarrativesPrintForm().getForm().getValues();
+        this.getNarrativesPrinter().loadPreview(formValues);
+    },
+
+    onNarrativesPrintPdfClick: function () {
+        var formValues = this.getNarrativesPrintForm().getForm().getValues();
+        this.getNarrativesPrinter().loadPrint(formValues);
+    },
+
+    onNarrativesPrintBrowserClick: function () {
+        var formValues = this.getNarrativesPrintForm().getForm().getValues();
+        this.getNarrativesPrinter().loadPreview(formValues, true);
+    },
+
+    onNarrativesClearFiltersClick: function () {
+        this.getNarrativesPrintForm().getForm().reset();
+    }
+});
