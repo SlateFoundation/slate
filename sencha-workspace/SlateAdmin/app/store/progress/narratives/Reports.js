dc589ab87e7b367dd7445ae314114c6c40e5d776
Bill Clinton <BillClinton@users.noreply.github.com>
2016-02-15 12:41:17 -0500

    Turn on remote filtering

--- sencha-workspace/SlateAdmin/app/store/progress/narratives/Reports.js
+++ sencha-workspace/SlateAdmin/app/store/progress/narratives/Reports.js
@@ -1,8 +1,9 @@
 /*jslint browser: true, undef: true, white: false, laxbreak: true *//*global Ext,Slate*/
-Ext.define('SlateAdmin.store.progress.narratives.Reports',{
+Ext.define('SlateAdmin.store.progress.narratives.Reports', {
     extend: 'Ext.data.Store',
 
     model: 'SlateAdmin.model.progress.narratives.Report',
     autoLoad: false,
-    autoSync: false
+    autoSync: false,
+    remoteFilter: true
 });
