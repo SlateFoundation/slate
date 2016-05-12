465935487a88249e08ae601f8ca8c03f98d4c504
Chris Alfano <chris@jarv.us>
2016-05-12 13:54:17 -0400

    Convert to git submodules for Sencha CMD packages
    Used commands:
    
        git submodule add -b master --name slate-theme https://github.com/SlateFoundation/slate-theme.git sencha-workspace/packages/slate-theme
    
        git submodule add -b ext/5/1/1/451 --name jarvus-hotfixes https://github.com/JarvusInnovations/sencha-hotfixes.git sencha-workspace/packages/jarvus-hotfixes
    
        git submodule add -b master --name jarvus-apikit https://github.com/JarvusInnovations/jarvus-apikit.git sencha-workspace/packages/jarvus-apikit
        git submodule add -b master --name emergence-apikit https://github.com/JarvusInnovations/emergence-apikit.git sencha-workspace/packages/emergence-apikit
        git submodule add -b master --name jarvus-ext-searchfield https://github.com/JarvusInnovations/jarvus-ext-searchfield.git sencha-workspace/packages/jarvus-ext-searchfield
        git submodule add -b master --name jarvus-ext-glyphs https://github.com/JarvusInnovations/jarvus-ext-glyphs.git sencha-workspace/packages/jarvus-ext-glyphs
        git submodule add -b master --name jarvus-ext-actionevents https://github.com/JarvusInnovations/jarvus-ext-actionevents.git sencha-workspace/packages/jarvus-ext-actionevents
        git submodule add -b ext/5/1/1/451 --name jarvus-ext-routing https://github.com/JarvusInnovations/jarvus-ext-routing.git sencha-workspace/packages/jarvus-ext-routing
        git submodule add -b ext/5/1/1/451 --name jarvus-lazydata https://github.com/JarvusInnovations/jarvus-lazydata.git sencha-workspace/packages/jarvus-lazydata
        git submodule add -b master --name jarvus-ext-treerecords https://github.com/JarvusInnovations/jarvus-ext-treerecords.git sencha-workspace/packages/jarvus-ext-treerecords
        git submodule add -b master --name jarvus-griderrors https://github.com/JarvusInnovations/jarvus-griderrors.git sencha-workspace/packages/jarvus-griderrors

--- sencha-workspace/packages/update-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for package in ./* ; do
-    if [ -d "$package/.git" ]; then
-        echo "Pulling $package from git"
-        cd "$package"
-        git pull
-        cd ..
-    fi
-done
