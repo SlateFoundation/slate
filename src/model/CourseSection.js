8f860ac212908ca6fd2a904af61033cecdbbf011
Chris Alfano <chris@jarv.us>
2016-05-14 05:23:30 +0000

    Refactor Section -> CourseSection

--- src/model/CourseSection.js
+++ src/model/CourseSection.js
@@ -1,5 +1,5 @@
 /*jslint browser: true, undef: true *//*global Ext*/
-Ext.define('Slate.model.Section', {
+Ext.define('Slate.model.CourseSection', {
     extend: 'Ext.data.Model',
     requires: [
         'Slate.proxy.Records',
---
+++ src/store/CourseSections.js
@@ -0,0 +1,7 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+Ext.define('Slate.store.CourseSections', {
+    extend: 'Ext.data.Store',
+
+
+    model: 'Slate.model.CourseSection'
+});
