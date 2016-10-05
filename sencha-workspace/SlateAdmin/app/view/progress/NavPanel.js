b8f64a718ba6217dc6084bd305a413f99c03193e
Chris Alfano <chris@jarv.us>
2016-10-05 17:13:17 -0400

    Delete cruft

--- sencha-workspace/SlateAdmin/app/view/progress/NavPanel.js
+++ sencha-workspace/SlateAdmin/app/view/progress/NavPanel.js
@@ -2,7 +2,8 @@
 Ext.define('SlateAdmin.view.progress.NavPanel', {
     extend: 'SlateAdmin.view.LinksNavPanel',
     xtype: 'progress-navpanel',
- //TODO: Delete extra link when nav panel arrow collapse is fixed
+
+
     title: 'Student Progress',
     data: true,
 
