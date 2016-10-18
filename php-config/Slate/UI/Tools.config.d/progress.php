c297e0d276429127d55f0200134bcd43f7be0a62
Chris Alfano <chris@jarv.us>
2016-10-18 12:35:36 -0400

    Update tool links for interim and term reports

--- php-config/Slate/UI/Tools.config.d/progress.php
+++ php-config/Slate/UI/Tools.config.d/progress.php
@@ -1,10 +1,15 @@
 <?php
 
-if (!empty($GLOBALS['Session']) && $GLOBALS['Session']->hasAccountLevel('Staff')) {
-    Slate\UI\Tools::$tools['Student Progress'] = [
-        'Narrative Reports' => [
-            '_href' => '/manage#progress/narratives',
-            '_icon' => 'documents'
-        ]
-    ];
+if (empty($GLOBALS['Session']) || !$GLOBALS['Session']->hasAccountLevel('Staff')) {
+    return;
 }
+
+Slate\UI\Tools::$tools['Student Progress']['Interim Reports'] = [
+    '_href' => '/manage#progress/interims/report',
+    '_icon' => 'interims'
+];
+
+Slate\UI\Tools::$tools['Student Progress']['Term Reports'] = [
+    '_href' => '/manage#progress/terms/report',
+    '_icon' => 'documents'
+];
