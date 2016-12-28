983f22dd8f6e485540a588ea064853241159f536
Nafis Bey <nafis@jarv.us>
2016-12-28 12:18:36 -0500

    Remove jslint config

--- sencha-workspace/SlateAdmin/app/view/progress/SectionNotesForm.js
+++ sencha-workspace/SlateAdmin/app/view/progress/SectionNotesForm.js
@@ -1,4 +1,3 @@
-/*jslint browser: true, undef: true, white: false, laxbreak: true *//*global Ext,Slate*/
 Ext.define('SlateAdmin.view.progress.SectionNotesForm', {
     extend: 'Ext.form.Panel',
     xtype: 'progress-sectionnotesform',
