52c4c380c5b731ca2092ab6363b3a3309376ac4c
Chris Alfano <chris@jarv.us>
2017-11-30 22:43:10 -0500

    Finish participant store/model

--- src/store/courses/SectionParticipants.js
+++ src/store/courses/SectionParticipants.js
@@ -5,8 +5,55 @@ Ext.define('Slate.store.courses.SectionParticipants', {
     model: 'Slate.model.course.SectionParticipant',
 
     config: {
+        section: null,
+        cohort: null,
+        role: null,
+
         pageSize: 0,
+        remoteSort: false,
         sorters: [
+            {
+                property: 'LastName',
+                direction: 'ASC'
+            },
+            {
+                property: 'FirstName',
+                direction: 'ASC'
+            }
         ]
+    },
+
+
+    constructor: function() {
+        this.callParent(arguments);
+        this.dirty = true;
+    },
+
+
+    // config handlers
+    updateSection: function(section) {
+        this.getProxy().setExtraParam('course_section', section || null);
+        this.dirty = true;
+    },
+
+    updateCohort: function(cohort) {
+        this.getProxy().setExtraParam('cohort', cohort || null);
+        this.dirty = true;
+    },
+
+    updateRole: function(role) {
+        this.getProxy().setExtraParam('role', role || null);
+        this.dirty = true;
+    },
+
+
+    // member methods
+    loadIfDirty: function() {
+        if (!this.dirty) {
+            return;
+        }
+
+        this.dirty = false;
+        this.load();
     }
 });
