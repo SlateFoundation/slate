97546dc05a792a4a677103117e623199bb1afd78
Chris Alfano <chris@jarv.us>
2019-06-10 20:46:17 -0700

    feat: enable arrayValues for Google sheet imports

--- php-classes/Slate/Connectors/GoogleSheets/Connector.php
+++ php-classes/Slate/Connectors/GoogleSheets/Connector.php
@@ -46,10 +46,14 @@ class Connector extends \Slate\Connectors\AbstractSpreadsheetConnector implement
 
 
         // execute tasks based on available spreadsheets
+        $readerOptions = [
+            'arrayValues' => true
+        ];
+
         if (!empty($Job->Config['studentsCsv'])) {
             $results['pull-students'] = static::pullStudents(
                 $Job,
-                SpreadsheetReader::createFromStream(fopen($Job->Config['studentsCsv'], 'r')),
+                SpreadsheetReader::createFromStream(fopen($Job->Config['studentsCsv'], 'r'), 'text/csv', $readerOptions),
                 $pretend
             );
         }
@@ -57,7 +61,7 @@ class Connector extends \Slate\Connectors\AbstractSpreadsheetConnector implement
         if (!empty($Job->Config['alumniCsv'])) {
             $results['pull-alumni'] = static::pullAlumni(
                 $Job,
-                SpreadsheetReader::createFromStream(fopen($Job->Config['alumniCsv'], 'r')),
+                SpreadsheetReader::createFromStream(fopen($Job->Config['alumniCsv'], 'r'), 'text/csv', $readerOptions),
                 $pretend
             );
         }
@@ -65,7 +69,7 @@ class Connector extends \Slate\Connectors\AbstractSpreadsheetConnector implement
         if (!empty($Job->Config['staffCsv'])) {
             $results['pull-staff'] = static::pullStaff(
                 $Job,
-                SpreadsheetReader::createFromStream(fopen($Job->Config['staffCsv'], 'r')),
+                SpreadsheetReader::createFromStream(fopen($Job->Config['staffCsv'], 'r'), 'text/csv', $readerOptions),
                 $pretend
             );
         }
@@ -73,7 +77,7 @@ class Connector extends \Slate\Connectors\AbstractSpreadsheetConnector implement
         if (!empty($Job->Config['sectionsCsv'])) {
             $results['pull-sections'] = static::pullSections(
                 $Job,
-                SpreadsheetReader::createFromStream(fopen($Job->Config['sectionsCsv'], 'r')),
+                SpreadsheetReader::createFromStream(fopen($Job->Config['sectionsCsv'], 'r'), 'text/csv', $readerOptions),
                 $pretend
             );
         }
@@ -81,7 +85,7 @@ class Connector extends \Slate\Connectors\AbstractSpreadsheetConnector implement
         if (!empty($Job->Config['enrollmentsCsv'])) {
             $results['pull-enrollments'] = static::pullEnrollments(
                 $Job,
-                SpreadsheetReader::createFromStream(fopen($Job->Config['enrollmentsCsv'], 'r')),
+                SpreadsheetReader::createFromStream(fopen($Job->Config['enrollmentsCsv'], 'r'), 'text/csv', $readerOptions),
                 $pretend
             );
         }
