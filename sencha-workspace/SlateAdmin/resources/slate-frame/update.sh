4b14ea6b2ebf0f406e44e0d22eb8290d298d0bb2
Chris Alfano <chris@jarv.us>
2019-07-07 01:10:55 +0000

    fix: upgrade slate-frame

--- sencha-workspace/SlateAdmin/resources/slate-frame/update.sh
+++ sencha-workspace/SlateAdmin/resources/slate-frame/update.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+HOST=${1:-http://v2.slate.is}
+FASHION_REGEX='s/^(\$[^:]+:\s*)(.*?)\s+!default;/\1dynamic(\2);/'
 
-wget http://v2.slate.is/css/main.css -O main.css
+wget $HOST/css/main.css -qO main.css
 
-mkdir img
-wget http://v2.slate.is/img/slate-logo-white.svg -O img/slate-logo-white.svg
-wget http://v2.slate.is/img/slate-icons/slate-icons.svg -O img/slate-icons.svg
+mkdir -p img
+wget $HOST/img/logo.png -qO img/logo.png
+wget $HOST/img/slate-logo-white.svg -qO img/slate-logo-white.svg
+wget $HOST/img/slate-icons/slate-icons.svg -qO img/slate-icons.svg
 
-mkdir fonts
-wget http://v2.slate.is/css/fonts/lato.css -O fonts/lato.css
-wget http://v2.slate.is/css/fonts/sanchez.css -O fonts/sanchez.css
-wget http://v2.slate.is/css/fonts/font-awesome.css -O fonts/font-awesome.css
+mkdir -p fonts
+wget $HOST/css/fonts/lato.css -qO fonts/lato.css
+wget $HOST/css/fonts/sanchez.css -qO fonts/sanchez.css
+wget $HOST/css/fonts/font-awesome.css -qO fonts/font-awesome.css
+
+mkdir -p sass/site sass/slate sass/skeleton
+(wget $HOST/sass/_variables-all.scss -qO- && echo) | perl -e 'print reverse <>' > sass/_variables-all.scss
+wget $HOST/sass/site/_variables.scss -qO- | perl -pe $FASHION_REGEX > sass/site/_variables.scss
+wget $HOST/sass/slate/_variables.scss -qO- | perl -pe $FASHION_REGEX > sass/slate/_variables.scss
+wget $HOST/sass/skeleton/_variables.scss -qO- | perl -pe $FASHION_REGEX > sass/skeleton/_variables.scss
