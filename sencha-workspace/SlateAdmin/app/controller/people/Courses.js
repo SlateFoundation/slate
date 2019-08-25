c1d24171ed88e24f00c723cfdb1b201ef9217e4e
Chris Alfano <chris@jarv.us>
2019-08-25 15:13:15 +0000

    fix: handle comboxbox getValue returning an ID

--- sencha-workspace/SlateAdmin/app/controller/people/Courses.js
+++ sencha-workspace/SlateAdmin/app/controller/people/Courses.js
@@ -64,19 +64,23 @@ Ext.define('SlateAdmin.controller.people.Courses', {
             return;
         }
 
-        if (!selectedTerm) {
-            selectedTerm = termsStore.getCurrentTerm();
+        if (
+            !selectedTerm
+            && (selectedTerm = termsStore.getCurrentTerm())
+        ) {
+            // push selected term to combo
+            termSelector.setSelection(selectedTerm);
+
+            // switch to int
+            selectedTerm = selectedTerm.getId();
         }
 
         coursesPanel.setLoading(false);
 
         // configure proxy and load store
         personSectionsProxy.url = '/people/' + person.get('ID') + '/courses';
-        personSectionsProxy.setExtraParam('termID', selectedTerm ? selectedTerm.getId() : null);
+        personSectionsProxy.setExtraParam('termID', selectedTerm || null);
         personSectionsStore.load();
-
-        // push selected term to combo
-        termSelector.setValue(selectedTerm);
     },
 
     onCourseTermSelect: function(field, newValue, oldValue) {
