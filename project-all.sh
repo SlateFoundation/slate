e033e189f60fcd40037d116fb6f41c24dbe5feef
Chris Alfano <chris@jarv.us>
2019-08-25 23:17:39 -0400

    chore: remove emergence-vfs-skeleton projection

--- project-all.sh
+++ project-all.sh
@@ -9,8 +9,3 @@ git holo project emergence-vfs-site \
     --fetch \
     --ref=origin/releases/v2 \
     --commit-to=emergence/vfs-site/v2
-
-git holo project emergence-vfs-skeleton \
-    --fetch \
-    --ref=origin/releases/v2 \
-    --commit-to=emergence/vfs-skeleton/v2
