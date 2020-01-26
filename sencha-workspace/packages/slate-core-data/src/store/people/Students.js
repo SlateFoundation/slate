95b3f1b8b83a6c0248f57fb25557147c91ad60d1
Chris Alfano <chris@jarv.us>
2020-01-26 19:13:37 +0000

    refactor: add core people store

--- sencha-workspace/packages/slate-core-data/src/store/people/Students.js
+++ sencha-workspace/packages/slate-core-data/src/store/people/Students.js
@@ -1,17 +1,10 @@
 Ext.define('Slate.store.people.Students', {
-    extend: 'Ext.data.Store',
+    extend: 'Slate.store.people.People',
 
 
-    model: 'Slate.model.person.Person',
     config: {
         list: null,
 
-        pageSize: 0,
-        remoteSort: false,
-        sorters: [{
-            property: 'SortName',
-            direction: 'ASC'
-        }],
         proxy: {
             type: 'slate-people',
             url: '/people/*students',
@@ -20,31 +13,9 @@ Ext.define('Slate.store.people.Students', {
     },
 
 
-    constructor: function() {
-        this.callParent(arguments);
-        this.dirty = true;
-    },
-
-
     // config handlers
     updateList: function(list) {
         this.getProxy().setExtraParam('list', list || null);
         this.dirty = true;
-    },
-
-
-    // member methods
-    loadIfDirty: function() {
-        if (!this.dirty) {
-            return;
-        }
-
-        this.dirty = false;
-        this.load();
-    },
-
-    unload: function() {
-        this.loadCount = 0;
-        this.removeAll();
     }
 });
